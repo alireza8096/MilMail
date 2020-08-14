22a99d783ac496d9a3fb10c31a97124ab8bcb564
Alireza Hosseinkhani <alireza.8096@chmail.ir>
2020-08-14 15:08:58 +0430

    initializing

---
+++ app/src/main/java/com/example/milmail/emailList.java
@@ -0,0 +1,23 @@
+package com.example.milmail;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class emailList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_list);
+        String username = getIntent().getStringExtra("username");
+
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        EmailAdapter adapter = new EmailAdapter(this, Email.getAllEmails());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
